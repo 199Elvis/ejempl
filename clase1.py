deb096e57300013e00f5bc7ecb44cfeba42eb201
199Elvis <destroyer65378405@gmail.com>
2024-02-14 17:31:01 -0400

    Proyect start

---
+++ clase1.py
@@ -0,0 +1,7 @@
+print('hola mundo')
+nombre, apellido = "Elvis", "Vedia"
+print(nombre, apellido)
+print(nombre.capitalize(), apellido.title())
+print(nombre.upper(), apellido.lower())
+print(f'nombre:{nombre} , apellido: {apellido}')
+print(f'nombre:{nombre}\napellido: {apellido}')
